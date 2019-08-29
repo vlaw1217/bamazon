7c7c5442f3f4593148b5ec33ed2864e560ee9e0d
Vicky Law <vicky.law1217@gmail.com>
2019-08-29 15:31:26 -0400

    inserted products data

--- bamazon.sql
+++ bamazon.sql
@@ -11,11 +11,44 @@ USE bamazon_db;
 CREATE TABLE products (
 
 -- Unique id for each product --
-ID INTEGER Auto_increment NOT NULL,
+ID INTEGER auto_increment NOT NULL,
 Name_of_Product VARCHAR(100) NOT NULL,
 Department_Name VARCHAR(100) NOT NULL,
-Price DECIMAL(10,2),
+Price DECIMAL (10,2),
 Stock_Quantity INTEGER,
 PRIMARY KEY (id)
 
 );
+
+-- Products data --
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Foundation', 'Mississauga', 56, 70);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Mascara', 'Richmond Hill', 17, 44);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Lipstick', 'Milton', 42, 102);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Eyeshadow', 'Oakville', 86, 119);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Blush', 'Brampton', 32, 63);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Concealer', 'Guelph', 42, 144);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Lip Gloss', 'Hamilton', 28, 29);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Nail Polish', 'Burlington', 26, 70);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Eyelash Curlers', 'Oakville', 20, 50);
+
+INSERT INTO products (Name_of_Product, Department_Name, Price, Stock_Quantity)
+VALUE ('Makeup Remover', 'Mississauga', 36, 123);
+
+
