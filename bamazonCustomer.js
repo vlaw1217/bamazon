d39142be287bbd1040002b4a1bd1f60f097106e7
Vicky Law <vicky.law1217@gmail.com>
2019-09-01 12:20:30 -0400

    confirm purchase and show total price

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,6 +29,7 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log(res);
+        console.log("------------------------------------------------------")
         userQuestion();
     })
 
@@ -40,43 +41,103 @@ function afterConnection() {
                 message: "Please input the product's ID number.",
                 name: "ID"
             },
-
             {
                 type: "input",
                 message: "How many unit?",
                 name: "number"
             },
         ])
+            
             .then(function (inquirerRes) {
                 if (inquirerRes.ID <= 10) {
                     //Show the item after user input id//
-                    connection.query("SELECT * FROM products where ID =" + inquirerRes.ID, function (err, res) {
+                    connection.query("SELECT * FROM products where ID = " + inquirerRes.ID, function (err, res) {
                         if (err) throw err;
                         console.log(res);
-                        //console.log(res[0].Stock_Quantity)
+                        console.log("------------------------------------------------------")
+                        //When user input quantity over the stock quanitity//
                         let data = res[0].Stock_Quantity;
-                        if (inquirerRes.number > data) {
+                        let unit = inquirerRes.number 
+                        if (unit > data) {
                             console.log("Sorry! This item is out of stock!")
-                        } else {               
-                            //update quantity to be updated
-                            let stockQuantity = data - inquirerRes.number
-                            //set the update query
-                            let updateQuantity = "UPDATE products SET Stock_Quantity = " + stockQuantity + " WHERE ID = " + inquirerRes.ID;
-                                console.log(updateQuantity)
-                                //start update product
-                                    connection.query(updateQuantity,
-                                        function (err) {
-                                            if (err) throw err;
-                                            console.log("Product updated");
-                                            }
-                                        );
-                                 //end update product
-                            
-                        }
-                        connection.end();
+                            /*If suffcicent stock is valid, ask user to confirm*/
+                        } else {
+
+                            /* before update the quantity, display the details before confirmation for update */
+                            connection.query("SELECT ID, Name_of_Product, Price FROM products where ID =" + inquirerRes.ID, function (err, res) {
+                                if (err) throw err;
+                                //let unit = inquirerRes.number 
+                                let price = res[0].Price * unit
+                                //console.log(price)
+                                console.log(res);
+                                console.log("Total unit to buy: " + unit)
+                                console.log("Total Price: " + price)
+                                console.log("-----------------------------------------------------")
+                            /* finish display */
+
+                            /* begin input prompt to confirm update the record */
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    message: "Please confirm your purchase.",
+                                    name: "confirm",
+                                    choices: ['Yes', 'No'],
+                                },
+                            ])
+                                .then(function (answer) {
+                                    console.log('Answer:', answer.confirm);
+                                    console.log("***Thank you for purchasing, see you next time!***")
+                                    console.log("-----------------------------------------------------")
+                                   
+                                        if (answer.confirm === "Yes") {
+                                          
+                                                //update quantity if user confirm//
+                                                let stockQuantity = data - inquirerRes.number
+                                                //set the update query//
+                                                let updateQuantity = "UPDATE products SET Stock_Quantity = " + stockQuantity + " WHERE ID = " + inquirerRes.ID;
+                                                console.log(updateQuantity)
+                                                //start update product//
+                                                connection.query(updateQuantity,
+                                                    function (err) {
+                                                        if (err) throw err;
+                                                        console.log("Product updated");
+                                                    }
+                                                
+                                                );
+                                           
+                                        connection.end();
+                                        //end update product//
+                                    } else {
+                                        //console.log("no")
+                                        inquirer.prompt([
+                                            {
+                                                type: "list",
+                                                message: "Do you want to exit or continue?",
+                                                name: "confirm",
+                                                choices: ['Exit', 'Continue'],
+                                            },
+                                        ])
+                                            .then(function (response) {
+                                                if (response.confirm === "Continue") {
+                                                    console.log('Response', "response.continue")
+                                                    afterConnection();
+                                                } else {
+                                                    console.log("Exit")
+                                                    process.exit();
+                                                }
+                                            })
+                                        
+                                    }
+                                });
+                        });
+                                
+                                }
+                        
+                        //connection.end();
                     })
-                    console.log("Thank you!");
+                    console.log("------------------------------------------------------");
                 }
+            
                 else if (inquirerRes.ID > 10) {
                     console.log("Sorry! No this product ID number!")
                     userQuestion();
@@ -85,19 +146,10 @@ function afterConnection() {
                     console.log("Error! Please input the product ID.")
                     userQuestion();
                 }
-
             });
+    
+    
     };
 
-    /*let confirmPayment = function () {
-        if (quantity === ture) {
-            
-        }
-    
-}*/
-
-
-};
-
-
+}
 
